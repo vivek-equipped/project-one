f0d1c3d39f02bf5b871ae012751d998e7764e63c
vivek-equipped <vivek.narain@equipped.ai>
2025-03-10 13:06:10 +0530

    first commit

---
+++ controllers/itemController.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require('mongodb');
+const url = 'mongodb+srv://adminuser:wyPCWvdlcoKDvZ61@minerva.jee9t.mongodb.net';  // MongoDB connection URL
+const dbName = 'crud-app';  // Database name
+
+async function getItems() {
+    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('items');  // The collection we are working with
+        const items = await collection.find().toArray();  // Fetch all items
+
+        return items;
+    } finally {
+        await client.close();
+    }
+}
+
+module.exports = {
+    getItems,
+};
